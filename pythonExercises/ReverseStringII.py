b2f722d5a5c0bff58f0086ea4ad412009c079de6
Manuel Alejandro Cladera Mamani <he.ale.cladera@gmail.com>
2026-07-12 15:08:51 -0400

    071220261508

--- pythonExercises/ReverseStringII.py
+++ pythonExercises/ReverseStringII.py
@@ -1,14 +1,8 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        s=list(s)
-        for i in range(0, len(s)-k+2, k):
-            j=i
-            l=min(i+k-1, len(s)-1)
-            while (j<l):
-                s[j], s[l]= s[l],s[j]
-                j+=1
-                l-=1
-        return "".join(s)
+        rs=''
+        for i in range(0, len(s), 2*k):
+            rs+= s[i:i+k][::-1]
+            rs+= s[i+k, i+2*k]
+        return rs
         
-s= Solution()
-print(s.reverseStr("abcdefg",2))
