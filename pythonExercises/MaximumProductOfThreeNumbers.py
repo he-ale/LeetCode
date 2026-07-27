fd448ed218ce4ccde2b48b5e4af23b2485d85188
Manuel Alejandro Cladera Mamani <he.ale.cladera@gmail.com>
2026-07-27 12:16:02 -0400

    072720261215

---
+++ pythonExercises/MaximumProductOfThreeNumbers.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        max1= max2= max3= -1001
+        min1, min2= 1001
+
+        for n in nums:
+            if (n > max1):
+                max1, max2, max3= n, max1, max2
+            elif (n > max2):
+                max2, max3= n, max2
+            elif (n > max3):
+                max3= n
+
+            if (n < min1):
+                min1, min2= n, min1
+            elif (n < min2):
+                min2= n
+
+        return max(max1*max2*max3, max1*min1*min2)
