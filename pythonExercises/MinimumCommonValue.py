02db10e65a829a3e3e065b1b0638d440d636e45a
Manuel Alejandro Cladera Mamani <he.ale.cladera@gmail.com>
2026-07-30 11:12:31 -0400

    073020261112

---
+++ pythonExercises/MinimumCommonValue.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        if (nums1[0] > nums2[-1] or nums2[0] > nums1[-1]):
+            return -1
+        i= j= 0;
+        while (i < len(nums1) and j < len(nums2)):
+            if (nums1[i] == nums2[j]):
+                return nums1[i]
+            elif (nums1[i] > nums2[j]):
+                j+=1
+            elif (nums1[i] < nums2[j]):
+                i+=1
