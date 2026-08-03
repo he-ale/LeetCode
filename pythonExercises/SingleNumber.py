c5df3e76e712adfc07623c281b421b01607ed5d2
Manuel Alejandro Cladera Mamani <he.ale.cladera@gmail.com>
2026-08-02 21:30:57 -0400

    080220262130

---
+++ pythonExercises/SingleNumber.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num= nums[0]
+        for i in range(1, len(nums)):
+            num= num ^ nums[i]
+        return num
